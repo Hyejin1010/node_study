3fb49f724015a66d7df9cb0cf6be9e43b6a275d7
Hyejin1010 <hyepago@naver.com>
2019-12-21 21:21:10 +0900

    path에 따라 파일을 읽고 반환하는 코드 추가

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var http = require("http");
+// var express = require('express');
 
 http.createServer(function(request, response) {
     /*
@@ -15,3 +16,9 @@ http.createServer(function(request, response) {
 }).listen(8081);
 
 console.log("Server running at http:127.0.0.1:8081");
+
+// events 모듈 사용
+var events = require('events');
+
+// EventEmitter 객체 생성
+var eventEmitter = new events.EventEmitter();
